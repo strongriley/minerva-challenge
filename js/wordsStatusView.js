00e392f1ece505986906a2c1c5f09c2ad745b937
Riley Strong <strongriley@gmail.com>
2013-03-15 16:24:59 -0700

    Improved error message if just opening webpage from file system.

--- js/wordsStatusView.js
+++ js/wordsStatusView.js
@@ -1,10 +1,15 @@
 WordsStatusView = Backbone.View.extend({
     initialize: function() {
         $(this.el).html("Loading...");
-        this.model.on('change', this.ready, this);
+        this.model.on('change:tree', this.ready, this);
+        this.model.on('error', this.error, this);
     },
 
     ready: function() {
         $(this.el).html("Ready");
+    },
+
+    error: function() {
+        $(this.el).html("Could not load words. Make sure to load from the SimpleHTTPServer");
     },
 });
