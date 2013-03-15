e4c7670276c03555f571a9943d2f52423a2c29f4
Riley Strong <strongriley@gmail.com>
2013-03-15 14:38:11 -0700

    parse & build initial letter tree structure

--- js/wordsModel.js
+++ js/wordsModel.js
@@ -6,25 +6,54 @@ WordsModel = Backbone.Model.extend({
         // Pre-populate everything
         this.fetch({
             dataType: "text",
-            success: this.success,
             error: this.error
         });
     },
 
     parse: function(text, options) {
-        var self, words, tree;
-        self = this;
+        /* Builds a tree out of each letter.
+         * For example, the word "and" could be found at position...
+         * a.children.n.children.d
+         * 
+         * This will make finding matching words much more straight-forward.
+         */
+        var words, tree;
         words = text.split("\n");
+        // TODO debug
+        var limit = 100;
+        var j = 0;
+
+        tree = {};
         _.each(words, function(word) {
-            // TODO build tree
+            // TODO debugging code
+            if (j > limit) {
+                return;
+            }
+            j++;
+
+            word = word.toLowerCase();
+
+            // TODO more efficient to keep track of position in the tree.
+            var pos = tree;  // Always back to the top for each new word.
+
+            var i = 0;
+            var length = word.length;
+            _.each(word, function(letter) {
+                // increment beforehand for simpler determination that we're
+                // at the end.
+                i++;
+                if (pos[letter] == undefined) {
+                    pos[letter] = {children: {}};
+                }
+                if (i == length) {
+                    pos.word = true;
+                } else {
+                    pos = pos[letter].children;
+                }
+            });
         });
 
         return tree;
-    },
-
-
-    success: function(model, response, options) {
-        console.log(response);
     },
 
     error: function(model, xhr, options) {
