d089d4f72ab40d3db06554ed2d31de8fccf35990
Riley Strong <strongriley@gmail.com>
2013-03-15 17:49:41 -0700

    Improve speed: only 1 update to UI once search is complete

--- js/matchesView.js
+++ js/matchesView.js
@@ -1,15 +1,7 @@
 MatchesView = Backbone.View.extend({
     initialize: function() {
-        this.collection.on('add', this.appendMatch, this);
         this.collection.on('reset', this.reset, this);
         this.model.on('searchComplete', this.searchComplete, this);
-    },
-
-    appendMatch: function(model, collection, options) {
-        if (collection.length === 1) {
-            $(this.el).html('');
-        }
-        $(this.el).append(model.get('id') + "<br>");
     },
 
     reset: function() {
@@ -17,8 +9,15 @@ MatchesView = Backbone.View.extend({
     },
 
     searchComplete: function() {
+        var self = this;
+        var html = "";
         if (this.collection.length === 0) {
             $(this.el).html("No matches.");
+        } else {
+            _.each(this.collection.models, function(model) {
+                html += model.get('id') + "<br>"
+            });
+            $(self.el).html(html);
         }
     }
 });
