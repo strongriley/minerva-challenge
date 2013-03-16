4d6ed4f9b218ca2789f46d14946e3d348e06c895
Riley Strong <strongriley@gmail.com>
2013-03-15 18:34:16 -0700

    CSS cleanup showing ready

--- static/js/WordsStatusView.js
+++ static/js/WordsStatusView.js
@@ -1,15 +1,18 @@
 WordsStatusView = Backbone.View.extend({
     initialize: function() {
-        $(this.el).html("Loading...");
+        $(this.el).addClass("loading");
+        this.$('p').html("Loading...");
         this.model.on('change:tree', this.ready, this);
         this.model.on('error', this.error, this);
     },
 
     ready: function() {
-        $(this.el).html("Ready");
+        $(this.el).removeClass("loading error").addClass("ready");
+        this.$('p').html("Ready");
     },
 
     error: function() {
-        $(this.el).html("Could not load words. Make sure to load from the SimpleHTTPServer");
+        $(this.el).removeClass("loading ready").addClass("error");
+        this.$('p').html("Could not load words. Make sure to load from the SimpleHTTPServer");
     },
 });
