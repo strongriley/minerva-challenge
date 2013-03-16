0580ec330112eb3d9a6e7ce7ec47156e9b6db6eb
Riley Strong <strongriley@gmail.com>
2013-03-15 17:52:08 -0700

    <li>'s for matched words

--- js/matchesView.js
+++ js/matchesView.js
@@ -10,13 +10,14 @@ MatchesView = Backbone.View.extend({
 
     searchComplete: function() {
         var self = this;
-        var html = "";
         if (this.collection.length === 0) {
             $(this.el).html("No matches.");
         } else {
+            var html = "<ul>";
             _.each(this.collection.models, function(model) {
                 html += model.get('id') + "<br>"
             });
+            html += "</ul>";
             $(self.el).html(html);
         }
     }
